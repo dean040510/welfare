b29dc1279f715ed7b6eda446c095a63819181718
dean040510 <b01705049@ntu.edu.tw>
2016-05-22 03:48:37 +0800

    首頁完工

--- js/main.js
+++ js/main.js
@@ -1,171 +1,108 @@
-jQuery(function($) {
-	"use strict";
-	// Author Code Here
 
-	var owlPricing;
-	var ratio = 2;
-
-	// Window Load
-	$(window).load(function() {
-		// Preloader
-		$('.intro-tables, .parallax, header').css('opacity', '0');
-		$('.preloader').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-			$('.preloader').hide();
-			$('.parallax, header').addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-				$('.intro-tables').addClass('animated fadeInUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend');
-			});
-		});
-
-		// Header Init
-		if ($(window).height() > $(window).width()) {
-			var ratio = $('.parallax').width() / $('.parallax').height();
-			$('.parallax img').css('height', ($(window).height()) + 'px');
-			$('.parallax img').css('width', $('.parallax').height() * ratio + 'px');
-		}
-
-		$('header').height($(window).height() + 80);
-		$('section .cut').each(function() {
-			if ($(this).hasClass('cut-top'))
-				$(this).css('border-right-width', $(this).parent().width() + "px");
-			else if ($(this).hasClass('cut-bottom'))
-				$(this).css('border-left-width', $(this).parent().width() + "px");
-		});
-
-		// Sliders Init
-		$('.owl-schedule').owlCarousel({
-			singleItem: true,
-			pagination: true
-		});
-		$('.owl-testimonials').owlCarousel({
-			singleItem: true,
-			pagination: true
-		});
-		$('.owl-twitter').owlCarousel({
-			singleItem: true,
-			pagination: true
-		});
-
-		// Navbar Init
-		$('nav').addClass('original').clone().insertAfter('nav').addClass('navbar-fixed-top').css('position', 'fixed').css('top', '0').css('margin-top', '0').removeClass('original');
-		$('.mobile-nav ul').html($('nav .navbar-nav').html());
-		$('nav.navbar-fixed-top .navbar-brand img').attr('src', $('nav.navbar-fixed-top .navbar-brand img').data("active-url"));
-
-		// Typing Intro Init
-		$(".typed").typewriter({
-			speed: 60
-		});
-
-		// Popup Form Init
-		var i = 0;
-		var interval = 0.15;
-		$('.popup-form .dropdown-menu li').each(function() {
-			$(this).css('animation-delay', i + "s");
-			i += interval;
-		});
-		$('.popup-form .dropdown-menu li a').click(function(event) {
-			event.preventDefault();
-			$(this).parent().parent().prev('button').html($(this).html());
-		});
-
-		// Onepage Nav
-		$('.navbar.navbar-fixed-top .navbar-nav').onePageNav({
-			currentClass: 'active',
-			changeHash: false,
-			scrollSpeed: 400,
-			filter: ':not(.btn)'
-		});
-	});
-	// Window Scroll
-	function onScroll() {
-		if ($(window).scrollTop() > 50) {
-			$('nav.original').css('opacity', '0');
-			$('nav.navbar-fixed-top').css('opacity', '1');
-		} else {
-			$('nav.original').css('opacity', '1');
-			$('nav.navbar-fixed-top').css('opacity', '0');
-		}
-	}
-
-	window.addEventListener('scroll', onScroll, false);
-
-	// Window Resize
-	$(window).resize(function() {
-		$('header').height($(window).height());
-	});
-
-	// Pricing Box Click Event
-	$('.pricing .box-main').click(function() {
-		$('.pricing .box-main').removeClass('active');
-		$('.pricing .box-second').removeClass('active');
-		$(this).addClass('active');
-		$(this).next($('.box-second')).addClass('active');
-		$('#pricing').css("background-image", "url(" + $(this).data('img') + ")");
-		$('#pricing').css("background-size", "cover");
-	});
-
-	// Mobile Nav
-	$('body').on('click', 'nav .navbar-toggle', function() {
-		event.stopPropagation();
-		$('.mobile-nav').addClass('active');
-	});
-
-	$('body').on('click', '.mobile-nav a', function(event) {
-		$('.mobile-nav').removeClass('active');
-		if(!this.hash) return;
-		event.preventDefault();
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			event.stopPropagation();
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
-		}
-	});
-
-	$('body').on('click', '.mobile-nav a.close-link', function(event) {
-		$('.mobile-nav').removeClass('active');
-		event.preventDefault();
-	});
-
-	$('body').on('click', 'nav.original .navbar-nav a:not([data-toggle])', function() {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			event.stopPropagation();
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1000);
-				return false;
-			}
-		}
-	});
-
-	function centerModal() {
-		$(this).css('display', 'block');
-		var $dialog = $(this).find(".modal-dialog"),
-			offset = ($(window).height() - $dialog.height()) / 2,
-			bottomMargin = parseInt($dialog.css('marginBottom'), 10);
-
-		// Make sure you don't hide the top part of the modal w/ a negative margin
-		// if it's longer than the screen height, and keep the margin equal to 
-		// the bottom margin of the modal
-		if (offset < bottomMargin) offset = bottomMargin;
-		$dialog.css("margin-top", offset);
-	}
-
-	$('.modal').on('show.bs.modal', centerModal);
-
-	$('.modal-popup .close-link').click(function(event){
-		event.preventDefault();
-		$('#modal1').modal('hide');
-	});
-
-	$(window).on("resize", function() {
-		$('.modal:visible').each(centerModal);
-	});
+$(function() {
+	$(".card1").hover(
+        function(){
+            $(".card1").parent().addClass("card-active");
+            $(".card1").parent().removeClass("card-inactive");
+            $(".card1 > img").attr("src","img/pic/low02.png");
+        },
+        function(){
+            $(".card1").parent().addClass("card-inactive");
+            $(".card1").parent().removeClass("card-active");
+            $(".card1 > img").attr("src","img/pic/low01.png");
+        }
+    );
+    $(".card2").hover(
+        function(){
+            $(".card2").parent().addClass("card-active");
+            $(".card2").parent().removeClass("card-inactive");
+            $(".card2 > img").attr("src","img/pic/fam02.png");
+        },
+        function(){
+            $(".card2").parent().addClass("card-inactive");
+            $(".card2").parent().removeClass("card-active");
+            $(".card2 > img").attr("src","img/pic/fam01.png");
+        }
+    );
+    $(".card3").hover(
+        function(){
+            $(".card3").parent().addClass("card-active");
+            $(".card3").parent().removeClass("card-inactive");
+            $(".card3 > img").attr("src","img/pic/stu02.png");
+        },
+        function(){
+            $(".card3").parent().addClass("card-inactive");
+            $(".card3").parent().removeClass("card-active");
+            $(".card3 > img").attr("src","img/pic/stu01.png");
+        }
+    );
+    $(".card4").hover(
+        function(){
+            $(".card4").parent().addClass("card-active");
+            $(".card4").parent().removeClass("card-inactive");
+            $(".card4 > img").attr("src","img/pic/old02.png");
+        },
+        function(){
+            $(".card4").parent().addClass("card-inactive");
+            $(".card4").parent().removeClass("card-active");
+            $(".card4 > img").attr("src","img/pic/old01.png");
+        }
+    );
+    $(".card5").hover(
+        function(){
+            $(".card5").parent().addClass("card-active");
+            $(".card5").parent().removeClass("card-inactive");
+            $(".card5 > img").attr("src","img/pic/disable02.png");
+        },
+        function(){
+            $(".card5").parent().addClass("card-inactive");
+            $(".card5").parent().removeClass("card-active");
+            $(".card5 > img").attr("src","img/pic/disable01.png");
+        }
+    );
+    $(".card6").hover(
+        function(){
+            $(".card6").parent().addClass("card-active");
+            $(".card6").parent().removeClass("card-inactive");
+            $(".card6 > img").attr("src","img/pic/aboriginal02.png");
+        },
+        function(){
+            $(".card6").parent().addClass("card-inactive");
+            $(".card6").parent().removeClass("card-active");
+            $(".card6 > img").attr("src","img/pic/aboriginal01.png");
+        }
+    );
+    $(".card7").hover(
+        function(){
+            $(".card7").parent().addClass("card-active");
+            $(".card7").parent().removeClass("card-inactive");
+            $(".card7 > img").attr("src","img/pic/soldier02.png");
+        },
+        function(){
+            $(".card7").parent().addClass("card-inactive");
+            $(".card7").parent().removeClass("card-active");
+            $(".card7 > img").attr("src","img/pic/soldier01.png");
+        }
+    );
+    $(".card8").hover(
+        function(){
+            $(".card8").parent().addClass("card-active");
+            $(".card8").parent().removeClass("card-inactive");
+            $(".card8 > img").attr("src","img/pic/immgrant02.png");
+        },
+        function(){
+            $(".card8").parent().addClass("card-inactive");
+            $(".card8").parent().removeClass("card-active");
+            $(".card8 > img").attr("src","img/pic/immgrant01.png");
+        }
+    );
+	$(".news > span").hover(
+		function(){
+            $(".news > span > a > img").attr("src","img/pic/moreinfo02.png");
+        },
+        function(){
+			$(".news > span > a > img").attr("src","img/pic/moreinfo01.png");
+        }
+	);
+    
 });
